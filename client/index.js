8ec6efb36d4312410a3ac58fcb8d242491fd98b0
r0mka <romanliveshere@gmail.com>
2020-12-24 21:40:07 -0800

    Add initial game state

--- client/index.js
+++ client/index.js
@@ -6,6 +6,32 @@ const gameScreen = document.getElementById('gameScreen');
 
 let canvas, ctx;
 
+const gameState = {
+  player: {
+    pos: {
+      x: 3,
+      y: 10,
+    },
+    vel: {
+      x: 1,
+      y: 0,
+    },
+    // object that contains x and y for every segment of the body of the snake
+    snake: [
+      { x: 1, y: 10 },
+      { x: 2, y: 10 },
+      { x: 3, y: 10 },
+    ],
+  },
+
+  food: {
+    x: 7,
+    y: 7,
+  },
+  // 20 squares in the game grid
+  gridSize: 20,
+};
+
 function init() {
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
