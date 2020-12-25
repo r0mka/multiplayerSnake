9063ba95cb5f8fb0edfe3381b7edcf56827a0277
r0mka <romanliveshere@gmail.com>
2020-12-24 21:47:09 -0800

    Paint snake onto the screen

--- client/index.js
+++ client/index.js
@@ -49,3 +49,29 @@ function init() {
 }
 
 init();
+
+function paintGame(state) {
+  ctx.fillStyle = BG_COLOUR;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  const food = state.food;
+
+  const gridSize = state.gridSize;
+  const size = canvas.width / gridSize;
+
+  ctx.fillStyle = FOOD_COLOUR;
+  ctx.fillRect(food.x * size, food.y * size, size, size);
+
+  paintPlayer(state.player, size, SNAKE_COLOUR);
+}
+
+function paintPlayer(playerState, size, colour) {
+  const snake = playerState.snake;
+
+  ctx.fillStyle = colour;
+  for (let cell of snake) {
+    ctx.fillRect(cell.x * size, cell.y * size, size, size);
+  }
+}
+
+paintGame(gameState);
